f544d6eb49bac3358b868f6f20f9d5f80f59bdeb
jov4n <jovan.bundalo@gmail.com>
2026-07-21 20:10:49 -0600

    Serve site from lain.xin apex with root base path.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://jov4n.github.io',
-	base: '/lain-xin/',
+	site: 'https://lain.xin',
+	base: '/',
 	output: 'static',
 });
